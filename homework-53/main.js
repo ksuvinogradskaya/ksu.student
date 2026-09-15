77786e99bcf6d92a3f7f3ded6f3e70a903faef2f
ksuvinogradskaya <ksu.vinogradskaya@gmail.com>
2026-09-15 11:44:50 +0300

    Add files via upload

---
+++ homework-53/main.js
@@ -0,0 +1,126 @@
+// Задание 1.
+// Дан массив пользователей:
+// const users = [
+//   { name: 'Alex', age: 24, isAdmin: false },
+//   { name: 'Bob', age: 13, isAdmin: false },
+//   { name: 'John', age: 31, isAdmin: true },
+//   { name: 'Jane', age: 20, isAdmin: false },
+//]
+// Добавьте в конец массива двух пользователей:
+// { name: 'Ann', age: 19, isAdmin: false },
+// { name: 'Jack', age: 43, isAdmin: true }
+
+// Задание 2.
+// Используя массив пользователей users из предыдущего задания, напишите функцию getUserAverageAge(users), которая возвращает средний возраст пользователей.
+
+// Задание 3.
+// Используя массив пользователей users из предыдущего задания, напишите функцию getAllAdmins(users), которая возвращает массив всех администраторов.
+
+// Задание 4.
+// Напишите функцию first(arr, n), которая возвращает первые n элементов массива. Если n == 0, возвращается пустой массив [], если n == undefined, то возвращается массив с первым элементом.
+
+//Задание 1.
+const users = [
+	{ name: "Alex", age: 24, isAdmin: false },
+	{ name: "Bob", age: 13, isAdmin: false },
+	{ name: "John", age: 31, isAdmin: true },
+	{ name: "Jane", age: 20, isAdmin: false },
+];
+
+users.push(
+	{
+		name: "Ann",
+		age: 19,
+		isAdmin: false,
+	},
+	{
+		name: "Jack",
+		age: 43,
+		isAdmin: true,
+	},
+);
+
+console.log(users);
+
+// Задание 2.
+const users1 = [
+	{ name: "Alex", age: 24, isAdmin: false },
+	{ name: "Bob", age: 13, isAdmin: false },
+	{ name: "John", age: 31, isAdmin: true },
+	{ name: "Jane", age: 20, isAdmin: false },
+	{ name: "Ann", age: 19, isAdmin: false },
+	{ name: "Jack", age: 43, isAdmin: true },
+];
+
+function getUserAverageAge(users1) {
+	let sum = 0;
+	for (let i = 0; i < users1.length; i++) {
+		sum += users1[i].age;
+	}
+	return sum / users1.length;
+}
+
+console.log(getUserAverageAge(users1));
+
+//Задача 3.
+const users3 = [
+	{ name: "Alex", age: 24, isAdmin: false },
+	{ name: "Bob", age: 13, isAdmin: false },
+	{ name: "John", age: 31, isAdmin: true },
+	{ name: "Jane", age: 20, isAdmin: false },
+	{ name: "Ann", age: 19, isAdmin: false },
+	{ name: "Jack", age: 43, isAdmin: true },
+];
+
+function forEach(array, callback) {
+	for (let i = 0; i < array.length; i++) {
+		callback(array[i]);
+	}
+}
+
+function getAllAdmins(users) {
+	const admins = [];
+	forEach(users, function (user) {
+		if (user.isAdmin === true) {
+			console.log(`Найден админ ${user.name}`);
+			admins.push(user);
+		}
+	});
+	return admins;
+}
+
+const result = getAllAdmins(users3);
+console.log("Итоговый массив админов:", result);
+
+// Задача 4
+const users4 = [
+	{ name: "Alex", age: 24, isAdmin: false },
+	{ name: "Bob", age: 13, isAdmin: false },
+	{ name: "John", age: 31, isAdmin: true },
+	{ name: "Jane", age: 20, isAdmin: false },
+	{ name: "Ann", age: 19, isAdmin: false },
+	{ name: "Jack", age: 43, isAdmin: true },
+];
+
+function first(arr, n) {
+	if (n === 0) {
+		return [];
+	}
+	if (n === undefined) {
+		return [arr[0]];
+	}
+	if (n !== 0 && n !== undefined && n > arr.length) {
+		console.error(`${n} не может быть больше длинны массива:${arr.length}`);
+		return;
+	}
+
+	const result1 = [];
+
+	for (let i = 0; i < n; i++) {
+		result1.push(arr[i]);
+	}
+
+	return result1;
+}
+
+console.log(first(users4, 8));
